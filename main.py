237740a3acca3ccdaff1258520eee35ae858cc48
flight04 <xshefcyk4633@stevenscollege.edu>
2024-04-25 08:46:40 -0400

    added chat

--- main.py
+++ main.py
@@ -30,17 +30,21 @@ messages = []
 def chat():
 	return render_template('chat.html')
 
-@app.route('/send_message', methods=['POST'])
-def send_message():
-	data = request.form
-	message = data['message']
-	messages.append({'text': message, 'user': 'customer'})
-	return 'Message sent'
+@app.route('/chat', defaults={'chat_type': 'returns'})
+@app.route('/chat/<chat_type>')
+def index(chat_type):
+    if request.method == 'POST':
+        message = request.form['message']
+        customer_id = 1
+        date = datetime.datetime.now()
 
-@app.route('/get_messages')
-def get_messages():
-	return jsonify({'messages': messages})
+        with engine.connect() as connection:
+            connection.execute(text("INSERT INTO Chat (message, dates, CustomerID, chat_type) VALUES (:message, :date, :customer_id, :chat_type)"), message=message, date=date, customer_id=customer_id, chat_type=chat_type)
 
+    with engine.connect() as connection:
+        messages = connection.execute(text("SELECT * FROM Chat WHERE chat_type = :chat_type"), chat_type=chat_type).fetchall()
+
+    return render_template('index.html', messages=messages, chat_type=chat_type)
 
 if __name__ == '__main__':
     app.run(debug=True)
